c5c42e75e600187e022f54799ca9fa644dc3bb39
patriciomashinkiash <morapatricio152@gmail.com>
2025-01-20 18:04:21 -0500

    App: Grafos - Recorridos DFS Y BFS

--- src/App.java
+++ src/App.java
@@ -135,25 +135,84 @@ public class App {
     }
 
     public static void runGraph() {
+        //NodeGraph node5 = grafo.addNode(5);
+        //NodeGraph node7 = grafo.addNode(7);
+        //NodeGraph node9 = grafo.addNode(9);
+        //NodeGraph node11 = grafo.addNode(11);
+        //NodeGraph node3 = grafo.addNode(3);
+        
+        //grafo.addEdge(node11, node3);
+        //grafo.addEdge(node3, node5);
+        //grafo.addEdge(node3, node9);
+        //grafo.addEdge(node5, node7);
+        //grafo.addEdge(node5, node9);
+        //grafo.addEdge(node5, node3);
+        
         Graph grafo = new Graph();
-        NodeGraph node5 = grafo.addNode(5);
-        NodeGraph node7 = grafo.addNode(7);
-        NodeGraph node9 = grafo.addNode(9);
-        NodeGraph node11 = grafo.addNode(11);
+
+        NodeGraph node0 = grafo.addNode(0);
+        NodeGraph node1 = grafo.addNode(1);
+        NodeGraph node2 = grafo.addNode(2);
         NodeGraph node3 = grafo.addNode(3);
-        
-        grafo.addEdge(node11, node3);
-        grafo.addEdge(node3, node5);
-        grafo.addEdge(node3, node9);
-        grafo.addEdge(node5, node7);
-        grafo.addEdge(node5, node9);
-        grafo.addEdge(node5, node3);
-        
+        NodeGraph node4 = grafo.addNode(4);
+        NodeGraph node5 = grafo.addNode(5);
 
-        
+        System.out.println("DIRECCIONAL:");
+        grafo.addEdgeUnit(node0, node3);
+        grafo.addEdgeUnit(node0, node5);
+        grafo.addEdgeUnit(node3, node2);
+        grafo.addEdgeUnit(node3, node4);
+        grafo.addEdgeUnit(node4, node1);
+        grafo.addEdgeUnit(node2, node1);
+        grafo.addEdgeUnit(node1, node0);
+
         grafo.printGraph();
+        System.out.println();
 
+        Graph grafo2 = new Graph();
+
+        NodeGraph g2node0 = grafo2.addNode(0);
+        NodeGraph g2node1 = grafo2.addNode(1);
+        NodeGraph g2node2 = grafo2.addNode(2);
+        NodeGraph g2node3 = grafo2.addNode(3);
+        NodeGraph g2node4 = grafo2.addNode(4);
+        NodeGraph g2node5 = grafo2.addNode(5);
+
+        System.out.println("NO DIRECCIONAL:");
+        grafo2.addEdge(g2node0, g2node3);
+        grafo2.addEdge(g2node0, g2node5);
+        grafo2.addEdge(g2node3, g2node2);
+        grafo2.addEdge(g2node3, g2node4);
+        grafo2.addEdge(g2node4, g2node1);
+        grafo2.addEdge(g2node2, g2node1);
+        grafo2.addEdge(g2node1, g2node0);
         
+        grafo2.printGraph();
+        System.out.println();
+
+        System.out.println("DIRECCIONAL:");
+        grafo.getDFS(node0);
+        grafo.getBFS(node0);
+        System.out.println();
+
+        System.out.println("\nNO DIRECCIONAL:");
+        grafo2.getDFS(g2node0);
+        grafo2.getBFS(g2node0);
+
+        Graph grafo3 = new Graph();
+        
+        NodeGraph g3node0 = grafo3.addNode(0);
+        NodeGraph g3node1 = grafo3.addNode(1);
+        NodeGraph g3node2 = grafo3.addNode(2);
+        NodeGraph g3node3 = grafo3.addNode(3);
+        NodeGraph g3node4 = grafo3.addNode(4);
+        NodeGraph g3node5 = grafo3.addNode(5);
+        NodeGraph g3node7 = grafo3.addNode(7);
+        NodeGraph g3node8 = grafo3.addNode(8);
+        NodeGraph g3node9 = grafo3.addNode(9);
+
+        System.out.println("----------------------------------------------------");
+        System.out.println("GRAFO #2");
 
     }
 }
--- src/main/materia/Controllers/Graph.java
+++ src/main/materia/Controllers/Graph.java
@@ -1,7 +1,12 @@
 package main.materia.Controllers;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+
 import main.materia.Models.NodeGraph;
 
 public class Graph {
@@ -22,25 +27,68 @@ public class Graph {
         dest.addNeighbor(src);
     }
 
+    public void addEdgeUnit(NodeGraph src, NodeGraph dest) {
+        src.addNeighbor(dest);
+    }
+
     public void printGraph() {
         for (NodeGraph nodeGraph : nodes) {
             System.out.print("Vertice " + nodeGraph.getValue() + ": ");
             for (NodeGraph neighbor : nodeGraph.getNeighbors()) {
-                System.out.print(neighbor.getValue() + " - ");
+                System.out.print(neighbor.getValue() + " -> ");
             }
             System.out.println() ;
         }
     }
 
-    public void getDFS(NodeGraph startNode) {
+    public void getDFS(NodeGraph start) {
+        Set<NodeGraph> visitados = new HashSet<>();
+        System.out.println("DFS desde el node " + start.getValue() + ":");
+        getDFSUtil(start, visitados);
+        System.out.println();
+    }
+
+    private void getDFSUtil(NodeGraph start, Set<NodeGraph> visitados) {
+        if (visitados.contains(start)) {
+            return;
+            
+        }
+
+        System.out.print(start.getValue() + " ");
+        visitados.add(start);
+
+        for (NodeGraph neighbor : start.getNeighbors()) {
+            getDFSUtil(neighbor, visitados);
+        }
+    }
+
+    public boolean getDFS(NodeGraph start, NodeGraph destino) {
+        
+    }
+
+    private boolean getDFSUtil(NodeGraph node, NodeGraph destino, Set<NodeGraph> visitados) {
 
     }
 
-    private void getDFSUtil(NodeGraph node, boolean[] visited) {
 
-    }
+    public void getBFS(NodeGraph start) {
+        Set<NodeGraph> visitados = new HashSet<>();
+        Queue<NodeGraph> cola = new LinkedList<>();
 
-    public void getBFS(NodeGraph startNode) {
+        System.out.println("BFS desde el nodo " + start.getValue() + ":");
+        cola.add(start);
+        visitados.add(start);
 
+        while (!cola.isEmpty()) {
+            NodeGraph actual = cola.poll();
+            System.out.print(actual.getValue() + " ");
+
+            for (NodeGraph neighbor : actual.getNeighbors()) {
+                if (!visitados.contains(neighbor)) {
+                    visitados.add(neighbor);
+                    cola.add(neighbor);
+                }
+            }
+        }
     }
 }
